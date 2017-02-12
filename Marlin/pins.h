18f505de397f5976e44d32a962e44d8a82a459d0
Martin Rice <mrice411@gmail.com>
2017-02-12 11:05:59 -0500

    split PICA pin definitions into two files corresponding to board revision

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -150,8 +150,10 @@
   #include "pins_MEGACONTROLLER.h"    // ATmega2560
 #elif MB(SCOOVO_X9H)
   #include "pins_SCOOVO_X9H.h"
-#elif MB(PICA) || MB(PICA_REVB)
+#elif MB(PICA) 
   #include "pins_PICA.h"
+#elif MB(PICA_REVB)
+  #include "pins_PICAOLD.h"
 #elif MB(GT2560_REV_A)
   #include "pins_GT2560_REV_A.h"      // ATmega1280, ATmega2560
 #elif MB(GT2560_REV_A_PLUS)
